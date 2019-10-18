db0cb796b76fbca3686764ba3abd3ade5e4ac5de
RafalWojciechRolsky <raaf.em@gmail.com>
2019-10-18 09:59:19 +0200

    Dodanie Redux Tools

--- front/src/components/store/index.js
+++ front/src/components/store/index.js
@@ -11,6 +11,10 @@ const reducer = (state = initialState, action) => {
 	return state
 }
 
-const store = createStore(reducer)
+// const store = createStore(reducer)
+
+const store = createStore(
+	reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
 export default store
